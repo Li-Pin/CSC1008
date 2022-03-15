0ea51fe830a7dd2756b55b3511047468e12b0e90
pepperso07 <2101771@sit.singaporetech.edu.sg>
2022-03-15 21:00:50 +0800

    Added database for Book and Shared ride

--- Website/models.py
+++ Website/models.py
@@ -3,7 +3,7 @@ from flask_login import UserMixin
 from sqlalchemy import func
 
 
-#user database
+# user database
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80))
@@ -12,10 +12,36 @@ class User(UserMixin, db.Model):
     rides = db.relationship('Rides')
 
 
-#rides database
+# rides database
 class Rides(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     fromLocation = db.Column(db.Float(100))
     toLocation = db.Column(db.Float(100))
-    date = db.Column(db.DateTime(timezone=True),default=func.now())
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey(User.id))
+
+
+# Book single ride database
+class BookRide(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    PickUp = db.Column(db.String(100))
+    DropOff = db.Column(db.String(100))
+    date = db.Column(db.date)
+    time = db.Column(db.time)
+    pax = db.column(db.Integer)
+    CarType = db.column(db.String(100))
+    Payment = db.column(db.String(100))
+
+
+# Book shared ride database
+class BookRide(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    FirstPickUp = db.Column(db.String(100))
+    SecondPickUp = db.Column(db.String(100))
+    FirstDropOff = db.Column(db.String(100))
+    FinalDropOff = db.Column(db.String(100))
+    date = db.Column(db.date)
+    time = db.Column(db.time)
+    pax = db.column(db.Integer)
+    CarType = db.column(db.String(100))
+    Payment = db.column(db.String(100))
