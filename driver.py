1a64803b5d28d9434e16bf6b05e68047cebecae2
Kenny <40017759+KL-2021@users.noreply.github.com>
2022-04-09 00:02:29 +0800

    Update driver.py

--- driver.py
+++ driver.py
@@ -1,13 +1,35 @@
+from sqlalchemy import null
 from graphADT import g
+import random
 
 
 class driver:
+
+    driver = {
+        1 : ["Peter Low", 91234457,"SKA1154C"],
+        2 : ["Alex Tan", 91234457,"SKA4554C"],
+        3 : ["Jess Low", 91234457,"SKA8888C"],
+        4 : ["Felix Tan", 88961234,"SKG4894C"],
+        5 : ["Lim Tan Lau", 89551445,"STH4354C"]
+    }
+
     def __init__(self):
         self.name = 'Ah teck'  # replace with forms.get.(driver name)
         self.id = 'ahteckSiol' # re
         self.start = None
         self.end = None
+        self.driverLocation = dict()
         self.pathRoute = []
+        self.assignDriverLocation()
+
+    def assignDriverLocation(self):
+        for i in range(len(self.driver)):
+            ranLocation = random.randint(0, len(g.locations))
+            if ranLocation not in self.driverLocation:
+                self.driverLocation.update({ranLocation: [i+1]})
+            else: 
+                self.driverLocation[ranLocation].append(i+1)
+        print(self.driverLocation)
 
     def startjob(self, start): # start should be key of location
         self.start = start
@@ -24,4 +46,6 @@ class driver:
 # C = g.dijkstra(newCustomer.start, newCustomer.end)
 # g.printSolution(C, g.parent, newCustomer.pathRoute)
 # maxDriverDistance = input('Enter max driver distance: ')
-# print(newCustomer.pathRoute)
+# print(newCustomer.pathRoute)clear
+
+driver()
